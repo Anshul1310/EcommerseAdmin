7bc12134c2599bfcad4c37a60a29b06c4150deb2
anshul <huntinggamingtech@gmail.com>
2021-08-20 12:55:49 +0530

    Partial design done

--- app/src/main/java/com/anstudios/ecommerseadmin/MainActivity.java
+++ app/src/main/java/com/anstudios/ecommerseadmin/MainActivity.java
@@ -8,20 +8,29 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.anstudios.ecommerseadmin.fragments.HomeFragment;
+import com.anstudios.ecommerseadmin.fragments.NotificationFragment;
+import com.anstudios.ecommerseadmin.fragments.OrdersFragment;
+import com.anstudios.ecommerseadmin.fragments.PincodeFragment;
+import com.anstudios.ecommerseadmin.fragments.ProductsFragment;
+import com.anstudios.ecommerseadmin.fragments.ProfileFragment;
+import com.anstudios.ecommerseadmin.fragments.SettingFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-
     private DrawerLayout drawerLayout;
-    private NavigationView navigationView;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +41,148 @@ public class MainActivity extends AppCompatActivity {
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.apptheme));
         drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_menu);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, null,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+        bottomNavigationView=findViewById(R.id.bottom_nav_main);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .replace(R.id.frame_layout, new ProfileFragment())
+                .commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.bottom_nav_pincode:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .replace(R.id.frame_layout, new PincodeFragment())
+                                .commit();
+                        break;
 
+                    case R.id.bottom_nav_products:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .replace(R.id.frame_layout, new ProductsFragment())
+                                .commit();
+                        break;
 
+                    case R.id.bottom_nav_orders:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .replace(R.id.frame_layout, new OrdersFragment())
+                                .commit();
+                        break;
+                    case R.id.bottom_nav_settings:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .replace(R.id.frame_layout, new SettingFragment())
+                                .commit();
+                        break;
+                    case R.id.bottom_nav_home:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                                .replace(R.id.frame_layout, new HomeFragment())
+                                .commit();
+                        break;
+
+                }
+                return true;
+            }
+        });
+        addNavListener();
     }
+
+    private void addNavListener(){
+        findViewById(R.id.nav_bar_home_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        .replace(R.id.frame_layout, new HomeFragment())
+                        .commit();
+            }
+        });
+        findViewById(R.id.nav_bar_products_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        .replace(R.id.frame_layout, new ProductsFragment())
+                        .commit();
+            }
+        });
+        findViewById(R.id.nav_bar_pincode_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        .replace(R.id.frame_layout, new PincodeFragment())
+                        .commit();
+            }
+        });
+        findViewById(R.id.nav_bar_orders_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        .replace(R.id.frame_layout, new OrdersFragment())
+                        .commit();
+            }
+        });
+        findViewById(R.id.nav_bar_notification_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        .replace(R.id.frame_layout, new NotificationFragment())
+                        .commit();
+            }
+        });
+        findViewById(R.id.nav_bar_settings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                        .replace(R.id.frame_layout, new SettingFragment())
+                        .commit();
+            }
+        });
+        findViewById(R.id.nav_bar_logout_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
 
     public void openNavDrawer(View view) {
         drawerLayout.openDrawer(GravityCompat.START);
--- app/src/main/java/com/anstudios/ecommerseadmin/fragments/SettingFragment.java
+++ app/src/main/java/com/anstudios/ecommerseadmin/fragments/SettingFragment.java
@@ -1,9 +1,11 @@
 package com.anstudios.ecommerseadmin.fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,12 +13,25 @@ import com.anstudios.ecommerseadmin.R;
 
 public class SettingFragment extends Fragment {
     private View view;
+    private ImageView editDialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view= inflater.inflate(R.layout.fragment_setting, container, false);
+        view = inflater.inflate(R.layout.fragment_setting, container, false);
+        editDialog = view.findViewById(R.id.edit_dialog_btn);
+        editDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                View vobj = LayoutInflater.from(getContext()).inflate(R.layout.layout_edit_details, null);
+                builder.setView(vobj);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.setCanceledOnTouchOutside(false);
+                alertDialog.show();
+            }
+        });
         return view;
     }
 }
